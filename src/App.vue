e378254114d9472e656870decbf1a15fbdaf0b26
Sabina Psuj <sabina.psuj@hatimeria.pl>
2021-10-28 15:17:22 +0200

    G4G-6: add needed assets, rename it and attach basic start for game

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
   <div id="app" class="container">
     <h1 class="title">Gra zombie</h1>
     <p class="mb-3">Spróbuj odgadnąć hasło, zanim dopadnie Cię zombie!</p>
+    <img :src="this.zombie" />
+    <b-button @click="startGame">START</b-button>
     <secret-word :secret="secret" />
     <div class="columns">
       <div class="column">
-        <letters-table />
+        <letters-table :action="action"/>
       </div>
       <div class="column">
         <p>Pomyłki: 0/6</p>
@@ -26,7 +28,45 @@ export default {
   },
   data: function () {
     return {
-      secret: 'apokalipsa zombie'
+      secret: 'apokalipsa zombie',
+      zombie: '',
+      count: 1,
+      zombieId: null
+    }
+  },
+  methods: {
+    setZombieIdleAnimation (id) {
+      this.zombie = `/assets/images/zombies/zombie${id}/zombie${id}_idle/zombie_${id}_idle_${this.count}.png`
+      this.count++
+
+      if (this.count === 13) {
+        this.count = 1
+      }
+    },
+    getZombie (animation, id) {
+      switch (animation) {
+        case 'idle':
+          setInterval(this.setZombieIdleAnimation(id), 750)
+          break;
+        case 'is-hited':
+          this.zombieIsHited(id)
+          break
+        default:
+          setInterval(this.setZombieIdleAnimation(id), 1000)
+        }
+    },
+    zombieIsHited (id) {
+      this.zombie = `/assets/images/zombies/zombie${id}/zombie${id}_gethit/zombie_${id}_gethit_${this.count}.png`
+      this.count++
+      this.getZombie('idle')
+
+      if (this.count === 9) {
+        this.count = 1
+      }
+    },
+    startGame () {
+      this.zombieId = Math.floor(Math.random() * (11 - 6) + 6)
+      this.getZombie('idle', this.zombieId)
     }
   }
 }
--- src/components/LettersTable.vue
+++ src/components/LettersTable.vue
@@ -1,17 +1,28 @@
 <template>
   <div>
-    <b-button v-for="letter in polishLetters" :key="letter" class="mx-1 my-1 is-uppercase">
+    <b-button 
+      @click="action"
+      type="is-dark"
+      v-for="letter in polishLetters"
+      :key="letter" 
+      class="mx-1 my-1 is-uppercase">
       {{ letter }}
     </b-button>
   </div>
 </template>
 
 <script>
+import { EventBus } from './../main.js'
 export default {
   name: 'Letters',
   data: function () {
     return {
       polishLetters: ['a', 'ą', 'b', 'c', 'ć', 'd', 'e', 'ę', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'ł', 'm', 'n', 'ń', 'o', 'ó', 'p', 'q', 'r', 's', 'ś', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'ź', 'ż']
+    }
+  },
+  methods: {
+    action () {
+      EventBus.$emit('hitZombie')
     }
   }
 }
